7ff0b03c0e931b5f37c023fedfc3e0fdba1b80e1
yuha1212 <yuzu09131208@gmail.com>
2025-08-15 09:03:43 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// 共通関数
+
+function getZukans() {
+  const json = localStorage.getItem('zukans');
+  return json ? JSON.parse(json) : [];
+}
+
+function saveZukans(zukans) {
+  localStorage.setItem('zukans', JSON.stringify(zukans));
+}
+
+function addZukan(name) {
+  const zukans = getZukans();
+  const newZukan = {
+    id: Date.now().toString(),
+    name: name,
+    photos: []
+  };
+  zukans.push(newZukan);
+  saveZukans(zukans);
+  return newZukan.id;
+}
+
+function getZukans() {
+  return JSON.parse(localStorage.getItem("zukans") || "[]");
+}
+
+function getZukanById(id) {
+  const zukans = getZukans();
+  return zukans.find(zukan => zukan.id === id);
+}
+
+function addPhotoToZukan(id, photoData) {
+  const zukans = getZukans();
+  const index = zukans.findIndex(z => z.id === id);
+  if (index === -1) return;
+
+  zukans[index].photos.push(photoData);
+  saveZukans(zukans);
+}
